21c1dc66201756d22a5b56089ddb8b7bca1b7ad5
hira sharker <hira_nak@yahoo.com>
2021-07-31 20:49:14 +0600

    correction in zone model ambiguous zone_id

--- application/models/zone_model.php
+++ application/models/zone_model.php
@@ -25,7 +25,7 @@ class Zone_Model extends CI_Model {
         $this->db->select('tbl_zone.*, ,tbl_employee.employee_name as zonal_head');
         $this->db->from('tbl_zone');
         $this->db->join('tbl_employee','tbl_employee.employee_id = tbl_zone.zhead_id', 'left');
-        $this->db->where('zone_id',$zone_id);
+        $this->db->where('tbl_zone.zone_id',$zone_id);
         $result_query=$this->db->get();
         $result=$result_query->row();
         return $result;
